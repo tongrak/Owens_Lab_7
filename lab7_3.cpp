f9a0f260585123d21a37bf22bdc76d3459166cfb
Oliver Tongrak <tongrak_ru@cmu.ac.th>
2020-12-29 15:20:34 +0700

    Lab7_3 done!!!

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -9,3 +9,42 @@
 "ONE-NIGHT-STAND"
 "MARRIED"
 */
+
+#include <iostream>
+using namespace std;
+
+int age, height;
+long int property;
+string status;
+//char dir;
+
+int main(){
+
+    cout << "Enter your age: ";
+    cin >> age;
+
+    if(age<=20){
+        cout << "Enter your height: ";
+        cin >> height;
+
+        if(height<160){status="UNFRIEND";}
+        else if(height<175){status="FRIEND";}
+        else{
+            cout << "Enter your property: ";
+            cin >> property;
+
+            if(property>69000000){status="MARRIED";}
+            else{status="ONE-NIGHT-STAND";}
+        }
+    }else if(age<30){
+        cout << "Enter your property: ";
+        cin >> property;
+
+        if(property>10000000){status="BEST FRIEND";}
+        else{status="UNFRIEND";}
+    }else{status="UNFRIEND";}
+
+    cout <<"Your status = "<<status;
+
+    return 0;
+}
